12206e89efc6971a822280e201b3596bea07d75e
jenisnm <136350163+jenisnm@users.noreply.github.com>
2023-07-24 12:44:51 +0530

    initial commit

---
+++ precache-manifest.cc6009133c84394badd5f3deca413378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6aef2f9518fa6df16b9373fd66fbb09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a98d6111585fb6ae8b14",
+    "url": "/static/css/main.81ea1f56.chunk.css"
+  },
+  {
+    "revision": "107f92b6ff7f3bda5d61",
+    "url": "/static/js/2.0c7e8b1f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0c7e8b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98d6111585fb6ae8b14",
+    "url": "/static/js/main.9fac5126.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9fac5126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
